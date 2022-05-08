e573a053e3d564562ecbc276d546fbe58d254a9c
splinshady <splin.shady@gmail.com>
2022-05-08 23:37:47 +0300

    feat: build keyboard from js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
+/* eslint-disable guard-for-in */
+/* eslint-disable no-restricted-syntax */
 import './sass/style.scss';
+import { keyCodes } from './js/keyCodes';
+import { Key } from './js/generateKey';
+
+const generateArrKey = (data) => {
+  const keyArr = [];
+  for (const key in data) {
+    keyArr.push(new Key(data[key]));
+  }
+  return keyArr;
+};
+
+const wrapper = document.querySelector('.keyboard');
+
+const renderKeyboard = () => {
+  wrapper.innerHTML = '';
+  const arrOfKey = generateArrKey(keyCodes);
+  for (let i = 0; i < arrOfKey.length; i += 1) {
+    wrapper.append(arrOfKey[i].generateArticle());
+  }
+};
+
+if (keyCodes) {
+  renderKeyboard();
+}
